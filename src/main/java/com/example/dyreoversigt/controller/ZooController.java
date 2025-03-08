459b2a3b8448d27e4fd716d574afad9c5bffae0e
PeyHunter <Nando.goat@gmail.com>
2025-03-08 14:27:08 +0100

    Creating zoo

--- src/main/java/com/example/dyreoversigt/controller/ZooController.java
+++ src/main/java/com/example/dyreoversigt/controller/ZooController.java
@@ -1,6 +1,6 @@
 package com.example.dyreoversigt.controller;
 
-import com.example.dyreoversigt.model.Zoo;
+import com.example.dyreoversigt.model.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -16,11 +16,57 @@ public class ZooController
 
     @GetMapping("/addAnimal")
 
-        public String showForm(Model model)
+    public String showForm(Model model)
+    {
+        model.addAttribute("zoo", zoo);
+        return "add-animal";
+    }
+
+    @PostMapping("/addAnimal")
+    public String addAnimal(@RequestParam String species,
+                            @RequestParam int maleCount,
+                            @RequestParam int femaleCount,
+                            Model model)
+    {
+        Animal animal = null;
+
+        switch (species)
         {
-            model.addAttribute("zoo", zoo);
-            return "add-animal";
+            case "Lion":
+                animal = (new Lion(maleCount, femaleCount));
+                break;
+            case "Tiger":
+                animal = (new Crocodile(maleCount, femaleCount));
+                break;
+            case "Elephant":
+                animal = (new Elephant(maleCount, femaleCount));
+                break;
+            case "Giraffe":
+                animal = (new Giraph(maleCount, femaleCount));
+                break;
+            case "Zebra":
+                animal = (new Zebra(maleCount, femaleCount));
+                break;
+            case "Panda":
+                animal = (new Hyena(maleCount, femaleCount));
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown species: " + species);
         }
+        zoo.addAnimals(animal);
+        model.addAttribute("animal", animal);
+        model.addAttribute("animals", zoo.getAnimals());
+        return "redirect:/addAnimal";
+    }
+
+    @GetMapping("/animal-added")
+    public String displayZoo(Model model)
+    {
+        model.addAttribute("animals", zoo.getAnimals());
+        return "add-animal";
+    }
 
 
 }
+
+
